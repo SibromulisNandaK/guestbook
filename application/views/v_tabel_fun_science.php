71069bb759ef41b5178947fc470a8c778d8bf56a
Suramon31 <49953449+Suramon31@users.noreply.github.com>
2019-09-25 16:37:55 +0700

    Update v_tabel_fun_science.php

--- application/views/v_tabel_fun_science.php
+++ application/views/v_tabel_fun_science.php
@@ -64,7 +64,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php $no=1; foreach($isi_tabel_fs as $isi):?>
+                                <?php $no=1; foreach($isi_tabel as $isi):?>
                                 <tr id="<?php echo $isi->id_undangan?>">
 
                                     <td><?php echo $no; $no++;?></td>
